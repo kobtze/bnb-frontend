937b623ed544f0f38030368c800beb70f2c8101f
dolev <dolevtsfoni1@gmail.com>
2020-07-16 20:07:45 +0300

    16/7/2020 20:07

--- src/views/BnbDetails.vue
+++ src/views/BnbDetails.vue
@@ -1,10 +1,44 @@
 <template>
-  
+  <div v-if="houseToShow"  class="house-details">
+    <h1>House Details:</h1>
+    <p>ID: {{houseToShow._id}}</P>
+    <p>Name: {{houseToShow.name}}</P>
+    <p>Price: {{houseToShow.price}}</P>
+    <p>Type: {{houseToShow.type}}</P>
+    <p>description: {{houseToShow.description}}</P>
+    <p>capacity: {{houseToShow.capacity}}</P>
+    
+  </div>
 </template>
 
 <script>
-export default {
 
+import {houseService} from '../services/house.service.js'
+
+export default {
+    data(){
+        return {
+            houseToShow: null
+        }
+    },
+    components: {
+
+    },
+     methods: {
+    async loadHouse(){
+           let {id} = this.$route.params;
+         try{
+           this.houseToShow= await houseService.getById(id)
+         }catch(err){
+             console.log('didnt get any house');
+         }
+             
+    },
+   
+},
+ created() {
+         this.loadHouse();
+    }
 }
 </script>
 
