2045b43f950843ce96069053f4650e6cc685ab07
Kobi Donner <kobibo@gmail.com>
2020-07-21 16:45:40 +0300

    Kobi Design 16:45 21/7

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,48 +1,79 @@
 <template>
   <section>
-    <Gallery :images="imgNewKey"></Gallery>
+    <!-- <Gallery :images="imgNewKey"></Gallery> -->
+    <div class="gallery-hero gallery-height">
+      <div class="gallery-grid">
+        <div :style="imgUrl1" class="placeholder image-1"></div>
+        <div :style="imgUrl2" class="placeholder image-2"></div>
+        <div :style="imgUrl3" class="placeholder image-3"></div>
+        <div :style="imgUrl4" class="placeholder image-4"></div>
+        <div :style="imgUrl5" class="placeholder image-5"></div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-
 // vue-cover-gallery
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Gallery from "vue-cover-gallery"
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Gallery from "vue-cover-gallery";
 
 export default {
-    name: 'ImageGallery',
-    props: ['images'],
-    computed: {
-
-        imgNewKey(){
-            var imgsArray = [];
-            var imgsFromProp = this.images;
-            for (var i = 0; i < imgsFromProp.length; i++) {
-                var imgObj = {
-                // title: i,
-                // description: `desc ${i}`,
-                href: imgsFromProp[i]
-                }
-                imgsArray.push(imgObj);
-            }
-            return imgsArray;
-        },
-        imgHrefs() {
-            const imgObj = {title: "", description: "", href: ""}
-            const map = this.images.map(image => {
-                const img = Object.create(imgObj);
-                img.href = image
-            })
-            return map 
-        },
+  name: "ImageGallery",
+  props: ["images"],
+  computed: {
+    imgUrl1() {
+        return `background: url("${this.images[0]}");
+                background-position: center center;
+                background-size: cover;`
     },
-    components: {
-        Gallery
+    imgUrl2() {
+    return `background: url("${this.images[1]}");
+            background-position: center center;
+            background-size: cover;`
+    },
+    imgUrl3() {
+    return `background: url("${this.images[2]}");
+            background-position: center center;
+            background-size: cover;`
+    },
+    imgUrl4() {
+    return `background: url("${this.images[3]}");
+            background-position: center center;
+            background-size: cover;`
+    },
+    imgUrl5() {
+    return `background: url("${this.images[4]}");
+            background-position: center center;
+            background-size: cover;`
+    },
+    imgNewKey() {
+      var imgsArray = [];
+      var imgsFromProp = this.images;
+      for (var i = 0; i < imgsFromProp.length; i++) {
+        var imgObj = {
+          // title: i,
+          // description: `desc ${i}`,
+          href: imgsFromProp[i]
+        };
+        imgsArray.push(imgObj);
+      }
+      return imgsArray;
+    },
+    imgHrefs() {
+      const imgObj = { title: "", description: "", href: "" };
+      const map = this.images.map(image => {
+        const img = Object.create(imgObj);
+        img.href = image;
+      });
+      return map;
     }
-}
+  },
+  components: {
+    // Gallery
+  }
+};
 </script>
 
 <style>
--- src/views/BnbDetails.vue
+++ src/views/BnbDetails.vue
@@ -1,13 +1,61 @@
 <template>
   <div v-if="houseToShow" class="house-details" :id="houseToShow._id">
     <section class="container">
-        <p class="name">{{houseToShow.name}}</P>
+
+        <section class="details-header">
+            <div class="house-name">{{houseToShow.name}}</div>
+            <!-- <p class="rating">{{houseToShow.scores.rating}}</p> -->
+            <div class="second-row">
+                <prev-scores :scores="houseToShow.scores.rating" :reviewcount="houseToShow.reviews.length" />
+                <span class="seperator" aria-hidden="true">·</span>
+                <div class="location">{{houseToShow.location.name}}</div>
+            </div>
+        </section>
+
         <image-gallery :images="houseToShow.imgUrls" />
-        <p>Price: {{houseToShow.price}}</P>
-        <p>Type: {{houseToShow.type}}</P>
-        <p class="description">description: {{houseToShow.description}}</P>
-        <p>capacity: {{houseToShow.capacity}}</P>
-        <amenities :amenities="houseToShow.amenities" />
+        <div class="details-main">
+            <div class="content-column">
+                <div class="content-header">
+                    {{houseToShow.type}} hosted by {{houseToShow.host.name}}
+                </div>
+                <div class="content-secondary-header">{{houseToShow.capacity}} guests</div>
+                <!-- <p>{{houseToShow.type}}</P> -->
+                <div class="content-desc">{{houseToShow.description}}</div>
+                <amenities :amenities="houseToShow.amenities" />
+            </div>
+            <div class="booking-column">
+                <div class="booking-sticky">
+                    <div style="border: 1px solid rgb(221, 221, 221); border-radius: 12px; padding: 24px; box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;">
+                        <div>
+                            <div class="1st-row-price-rating">
+                                <span class="_pgfqnw">$73</span>
+                                <span class="_1l0ezq0">/ night</span>
+                                <span class="_bq6krt">4.80 (70)</span>
+                            </div>
+                            <div class="2nd-row-checkin-chekout">
+                                <div class="_7eq2v2">Check-in</div>
+                                <div class="_7eq2v2">Checkout</div>
+                            </div>
+                            <div class="3rd-row-check-availabilty">
+                                <form action="/payments/book?hosting_id=31430309" method="post">
+                                    <input type="hidden" name="s" value="undefined">
+                                    <input type="hidden" name="hosting_id" value="31430309">
+                                    <input type="hidden" name="guest_currency" value="USD">
+                                    <input type="hidden" name="checkin" value="undefined">
+                                    <input type="hidden" name="checkout" value="undefined">
+                                    <input type="hidden" name="number_of_guests" value="1">
+                                    <input type="hidden" name="selected_cancellation_policy_id" value="undefined">
+                                    <button data-testid="homes-pdp-cta-btn" type="submit" class="_1ot1we5p">
+                                        <span class="_163rr5i">
+                                            <span class="_19di23v" style="background-position: calc((100 - var(--mouse-x, 0)) * 1%) calc((100 - var(--mouse-y, 0)) * 1%); --mouse-x:17.49; --mouse-y:11.4583;"></span></span><span class="_tcp689">Check availability </span>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <house-reviews :reviews="houseToShow.reviews" :scores="houseToShow.scores"/>
         <div class="google-map"><google-map :location="houseToShow.location" ></google-map></div>
         <date-picker />
@@ -18,11 +66,12 @@
 <script>
 
 import {houseService} from '../services/house.service.js';
-import GoogleMap from '@/components/GoogleMap.vue';
+import PrevScores from '@/components/PrevScores.vue';
 import ImageGallery from '@/components/ImageGallery.vue';
 import Amenities from '@/components/Amenities.vue';
 import HouseReviews from '@/components/HouseReviews.vue';
 import DatePicker from '@/components/DatePicker.vue';
+import GoogleMap from '@/components/GoogleMap.vue';
 
 export default {
     name: 'BnbDetails',
@@ -32,21 +81,21 @@ export default {
         }
     },
     components: {
+        PrevScores,
         ImageGallery,
         Amenities,
         HouseReviews,
-        GoogleMap,
         DatePicker,
+        GoogleMap,
     },
      methods: {
     async loadHouse(){
-           let {id} = this.$route.params;
-         try{
+        let {id} = this.$route.params;
+        try {
            this.houseToShow= await houseService.getById(id)
-         }catch(err){
-             console.log('didnt get any house');
-         }
-             
+        } catch(err) {
+            console.log('didnt get any house');
+        }
     },
    
 },
