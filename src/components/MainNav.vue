ce186b70c8a24106bbe80de6ef2f15c5d57bde4d
dolev <dolevtsfoni1@gmail.com>
2020-07-23 23:04:01 +0300

    MainNav 23:03 23/07 dolev

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,6 +1,5 @@
 <template>
   <section>
-    <div class="screen" @click="toggleMenu"></div>
     <header class="main-header container flex space-between align-center">
       <div class="flex-start flex">
         <div class="flex-start logo-symbol">
@@ -12,60 +11,97 @@
         </div>
         <div class="flex-end logo-word">airbnb</div>
       </div>
-      <ul class="main-nav flex align-center space-between  clean-list ">
-        
-          <svg
-            aria-hidden="true"
-            role="presentation"
-            viewBox="0 0 16 16"
-            xmlns="http://www.w3.org/2000/svg"
-            style="display: block; height: 16px; width: 16px; fill: currentcolor;"
-          >
-            <path
-              d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"
-            />
-          </svg>
-          <li>
-            <a href="#">host your home</a>
-          </li>
-          <li>
-            <a href="#">switch to hosting</a>
-          </li>
-          <li>
-            <a href="#">help</a>
-          </li>
-       
-        <div class="btn-container">
-          <button
-            class="btn-menu flex space-between"
-            type="button"
-            aria-expanded="false"
-            data-testid="cypress-headernav-profile"
-          >
-            <span  aria-label="Main navigation menu">
-              <svg
-                aria-hidden="true"
-                role="presentation"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-                style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"
-              >
-                <g fill="none" fill-rule="nonzero">
-                  <path d="m2 16h28" />
-                  <path d="m2 24h28" />
-                  <path d="m2 8h28" />
-                </g>
-              </svg>
-            </span>
-           
+      <ul class="main-nav flex align-center space-between clean-list">
+        <svg
+          aria-hidden="true"
+          role="presentation"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+          style="display: block; height: 16px; width: 16px; fill: currentcolor;"
+        >
+          <path
+            d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"
+          />
+        </svg>
+        <li>
+          <a href="#">host your home</a>
+        </li>
+        <li>
+          <a href="#">switch to hosting</a>
+        </li>
+        <li>
+          <a href="#">help</a>
+        </li>
+        <div class="btn-main-container">
+          <div class="btn-container">
+            <button
+              @click="onToggleMenu"
+              class="btn-menu flex space-between"
+              type="button"
+              aria-expanded="false"
+              data-testid="cypress-headernav-profile"
+            >
+              <span aria-label="Main navigation Screen">
+                <svg
+                  aria-hidden="true"
+                  role="presentation"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                  style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"
+                >
+                  <g fill="none" fill-rule="nonzero">
+                    <path d="m2 16h28" />
+                    <path d="m2 24h28" />
+                    <path d="m2 8h28" />
+                  </g>
+                </svg>
+              </span>
               <img
                 class="profile-picture"
                 src="https://a0.muscache.com/im/pictures/user/97e2b64e-61da-4d4b-953e-65d6dc0e92fd.jpg?aki_policy=profile_medium"
                 alt
               />
-           
-            <div class="notifications">3</div>
-          </button>
+
+              <div class="notifications">3</div>
+            </button>
+            <div v-show="toggleMenu" class="menu-modal">
+              <div class="login" @click="onToggleScreen">Login</div>
+              <div class="logout" @click="onToggleMenu">Sign up</div>
+              <div class="logout" @click="onToggleMenu">Logout</div>
+            </div>
+            <div  v-show="toggleScreen" class="post-modal">
+              <div class="login-modal">
+                <div class="form-group">
+                  <label for="usrname">
+                    <span class="glyphicon glyphicon-user"></span> Username
+                  </label>
+                  <input type="text" class="form-control" id="usrname"  placeholder="Enter email" />
+                </div>
+                <div class="form-group">
+                  <label for="psw">
+                    <span class="glyphicon glyphicon-eye-open"></span> Password
+                  </label>
+                  <input type="text" class="form-control" id="psw" placeholder="Enter password" />
+                </div>
+              </div>
+              <div class="close-modal" @click.stop="onToggleScreen">
+                <svg
+                  aria-label="Close"
+                  class="_8-yf5"
+                  fill="#ffffff"
+                  height="24"
+                  viewBox="0 0 48 48"
+                  width="24"
+                >
+                  <path
+                    clip-rule="evenodd"
+                    d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
       </ul>
     </header>
@@ -76,9 +112,21 @@
 
 <script>
 export default {
+  data() {
+    return {
+      toggleScreen: false,
+      toggleMenu: false
+    };
+  },
   methods: {
-    toggleMenu() {
-      document.body.classList.toggle("menu-open");
+    onToggleScreen() {
+      this.toggleMenu = false;
+      this.toggleScreen = !this.toggleScreen;
+      console.log(this.toggleScreen, " this.toggleScreen");
+    },
+    onToggleMenu() {
+      this.toggleMenu = !this.toggleMenu;
+      console.log(this.toggleMenu, " this.toggleMenu");
     }
   }
 };
