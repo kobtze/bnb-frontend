2c11c762dcdc0027636099868e417c82d9a2e80b
Kobi Donner <kobibo@gmail.com>
2020-07-25 17:45:23 +0300

    Responsive Kobi 17:45

--- src/components/HomePagePopularDest.vue
+++ src/components/HomePagePopularDest.vue
@@ -1,46 +1,47 @@
 <template>
+  <section class="home-page-popular-dest homepage-section container">
+    <h2>Destinations for future trips</h2>
 
-  <section class="home-page-popular-dest container">
-
-     <h2>Destinations for future trips</h2>
-  
-        <section class="img-container flex">
-
-        <section v-on:click="sendCategory('new york')">
-              <el-card class="card" :body-style="{ padding: '0px' }">
-              <img src="https://a0.muscache.com/im/pictures/55b99138-dc87-406a-86ef-89c2dc6f713b.jpg?aki_policy=large" class="image">
-              <div style="padding: 14px;">
-                  <p>New York</p>
-                  <div class="bottom clearfix">
-                  </div>
-              </div>
-            </el-card>
-       </section>
-
-        <section v-on:click="sendCategory('tokyo')">
-              <el-card class="card" :body-style="{ padding: '0px' }">
-              <img src="https://media.cntraveller.in/wp-content/uploads/2018/09/Tokyo-Shibuya-neon-lights-1366x768.jpg" class="image">
-              <div style="padding: 14px;">
-                  <p>Tokyo</p>
-                  <div class="bottom clearfix">
-                  </div>
-              </div>
-          </el-card>
-     </section>
-     
-       <section v-on:click="sendCategory('tel')">
-            <el-card  class="card" :body-style="{ padding: '0px' }">
-            <img src="https://israel.travel/wp-content/uploads/2019/03/jaffa-inside-min.jpg" class="image">
-            <div style="padding: 14px;">
-                <p>Tel Aviv</p>
-                <div class="bottom clearfix">
-                </div>
-            </div>
+    <section class="list-3">
+      <section v-on:click="sendCategory('new york')">
+        <el-card class="card" :body-style="{ padding: '0px' }">
+          <img
+            src="@/assets/img/new-york-small.jpg"
+            class="image"
+          />
+          <div style="padding: 14px;">
+            <p>New York</p>
+            <div class="bottom clearfix"></div>
+          </div>
         </el-card>
-     </section>
+      </section>
 
+      <section v-on:click="sendCategory('tokyo')">
+        <el-card class="card" :body-style="{ padding: '0px' }">
+          <img
+            src="@/assets/img/tokyo.jpg"
+            class="image"
+          />
+          <div style="padding: 14px;">
+            <p>Tokyo</p>
+            <div class="bottom clearfix"></div>
+          </div>
+        </el-card>
+      </section>
 
-        </section>
+      <section v-on:click="sendCategory('tel')">
+        <el-card class="card" :body-style="{ padding: '0px' }">
+          <img
+            src="@/assets/img/tel-aviv-small.jpg"
+            class="image"
+          />
+          <div style="padding: 14px;">
+            <p>Tel Aviv</p>
+            <div class="bottom clearfix"></div>
+          </div>
+        </el-card>
+      </section>
+    </section>
   </section>
 </template>
 
@@ -48,52 +49,51 @@
 export default {
   data() {
     return {
-     filterBy: {
+      filterBy: {
         location: "",
         checkIn: "",
         checkOut: "",
         adultNumber: "",
         childrenNumber: ""
-      },
+      }
     };
   },
-  methods:{
-    sendCategory(city){
-      console.log('in');
-         this.filterBy.location = city;
-         console.log(this.filterBy)
-         this.$emit("setFilter", this.filterBy);
-         this.$router.push("/app");
+  methods: {
+    sendCategory(city) {
+      console.log("in");
+      this.filterBy.location = city;
+      console.log(this.filterBy);
+      this.$emit("setFilter", this.filterBy);
+      this.$router.push("/app");
     }
   }
-          
-}
+};
 </script>
 
 
 <style>
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
-  
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  
-  .clearfix:after {
-      clear: both
-  }
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
 </style>
